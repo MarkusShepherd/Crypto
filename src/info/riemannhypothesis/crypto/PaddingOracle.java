fa193bb65f552b2c243ae91062d77b344f1269db
Markus Schepke <markus.schepke@censhare.com>
2014-01-30 10:24:25 +0000

    final refinement to PaddingOracle.java

--- src/info/riemannhypothesis/crypto/PaddingOracle.java
+++ src/info/riemannhypothesis/crypto/PaddingOracle.java
@@ -14,24 +14,41 @@ public class PaddingOracle {
 	public static void main(String[] args) throws IOException {
 
 		String cipher = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4";
+		final byte blockLength = 16;
 		ByteSequence bytes = ByteSequence.fromHexString(cipher);
 		ByteSequence plain = ByteSequence.EMPTY_SEQUENCE;
-		BlockSequence blocks = new BlockSequence(16, bytes);
+		BlockSequence blocks = new BlockSequence(blockLength, bytes);
 
-		for (int i = 3; i < blocks.length(); i++) {
+		for (int i = 1; i < blocks.length(); i++) {
 			ByteSequence iv = blocks.blockAt(i - 1);
 			ByteSequence block = blocks.blockAt(i);
+			boolean lastBlock = (i == blocks.length() - 1);
 			try {
-				plain = plain.append(decryptBlock(iv, block,
-						i == blocks.length() - 1));
+				ByteSequence plainBlock = decryptBlock(iv, block);
+				if (lastBlock) {
+					byte paddingLength = plainBlock
+							.byteAt(plainBlock.length() - 1);
+					if (paddingLength < 1 || paddingLength > blockLength) {
+						throw new Exception("Incorrect padding");
+					}
+					for (int pos = 1; pos < paddingLength; pos++) {
+						if (plainBlock.byteAt(plainBlock.length() - 1 - pos) != paddingLength) {
+							throw new Exception("Incorrect padding");
+						}
+					}
+					plainBlock = plainBlock.range(0, plainBlock.length()
+							- paddingLength);
+				}
+				System.out.println("The decryption of block " + i + " is: "
+						+ plainBlock.toString());
+				plain = plain.append(plainBlock);
 			} catch (Exception e) {
 				System.out.print("There was an error: " + e.getMessage());
 				e.printStackTrace();
 			}
 		}
-
-		System.out.println("Response: " + plain.toHexString());
-		System.out.println("Response: " + plain.toString());
+		System.out.println("Plaintext: " + plain.toString());
+		System.out.println("Plaintext (hex): " + plain.toHexString());
 	}
 
 	public static int getResponseCode(URL url) throws IOException {
@@ -44,12 +61,6 @@ public class PaddingOracle {
 	}
 
 	public static ByteSequence decryptBlock(ByteSequence iv, ByteSequence cipher)
-			throws CloneNotSupportedException, IOException {
-		return decryptBlock(iv, cipher, false);
-	}
-
-	public static ByteSequence decryptBlock(ByteSequence iv,
-			ByteSequence cipher, boolean lastBlock)
 			throws CloneNotSupportedException, IOException {
 		ByteSequence plain = new ByteSequence(new byte[cipher.length()]);
 		boolean wrongGuess = false;
